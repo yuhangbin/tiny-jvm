c52c2b46986d5738df7f09b83543ec50c967711c
yuhangbin <yuhangbinccc@gmail.com>
2023-10-09 17:18:16 +0800

    feat: instructions

---
+++ ch05/instructions/extended/goto_w.go
@@ -0,0 +1,19 @@
+package extended
+
+import (
+	"github.com/tiny/jvm/ch05/instructions/base"
+	"github.com/tiny/jvm/ch05/rtda"
+)
+
+// Branch always (wide index)
+type GOTO_W struct {
+	offset int
+}
+
+func (self *GOTO_W) FetchOperands(reader *base.BytecodeReader)  {
+	self.offset = int(reader.ReadInt32())
+}
+
+func (self *GOTO_W) Execute(frame *rtda.Frame)  {
+	base.Branch(frame, self.offset)
+}
--- ch05/instructions/extended/ifnull.go
+++ ch05/instructions/extended/ifnull.go
@@ -1,1 +1,18 @@
 package extended
+
+import (
+	"github.com/tiny/jvm/ch05/instructions/base"
+	"github.com/tiny/jvm/ch05/rtda"
+)
+
+// Branch if reference is null
+type IFNULL struct { base.BranchInstruction }
+// Branch if reference not null
+type IFNONNULL struct { base.BranchInstruction }
+
+func (self *IFNULL) Execute(frame *rtda.Frame)  {
+	ref := frame.OperandStack.PopRef()
+	if ref == nil {
+		base.Branch(frame, self.Offset)
+	}
+}
